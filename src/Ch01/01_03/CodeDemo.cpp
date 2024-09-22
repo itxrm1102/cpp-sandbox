c03d3552332972c4c59b3260195165eba5e92743
itxrm1102 <122915561+itxrm1102@users.noreply.github.com>
2024-09-22 17:37:17 +0000

    asking only for first name and print

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -6,9 +6,11 @@
 #include <string>
 
 int main(){
+    // only works for single words
     std::string str;
+    std::cout << "Enter your name: " << std::flush;
     std::cin >> str;
-    std::cout << str;
+    std::cout << "Nice to meet you, " << str << "." << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
